e90ab2c03c8ffa8740488508bdd59056e8a5d559
dborah123 <dborah@middlebury.edu>
2022-02-26 21:46:12 -0500

    trying to get junit5 working and testing repositories

--- src/test/kotlin/com/example/messagingapp/MessagingappApplicationTests.kt
+++ src/test/kotlin/com/example/messagingapp/MessagingappApplicationTests.kt
@@ -2,14 +2,34 @@ package com.example.messagingapp
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.assertj.core.api.Assertions.assertThat
 
 @SpringBootTest
 class MessagingappApplicationTests {
+
+	val underTest = Calculator()
 
 	@Test
 	fun contextLoads() {
 	}
 
+	@Test
+	fun itShouldAdd() {
+		// given
+		val a = 20
+		val b = 2
 
+		// when
+		val result = underTest.add(a, b)
+
+		// then
+		assertThat(result).isEqualTo(22)
+	}
+
+	class Calculator {
+		fun add(a: Int, b: Int): Int {
+			return a+b
+		}
+	}
 
 }
---
+++ src/test/kotlin/com/example/messagingapp/person/PersonRepositoryTest.kt
@@ -0,0 +1,17 @@
+package com.example.messagingapp.person
+
+import org.springframework.beans.factory.annotation.Autowired
+
+class PersonRepositoryTest {
+
+    @Autowired
+    val underTest: PersonRepository
+        get() {
+            TODO()
+        }
+
+    @Test
+    fun itShouldCheckIfPerson1Exists() {
+
+    }
+}
